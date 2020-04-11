3c3fef69ed334658a1613396537724584fa93353
JesusQuinto <jesuskinto@gmail.com>
2020-04-11 11:13:59 -0400

    Actualizacion para Now

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ const config = {
     db_password: process.env.DB_PASSWORD,
     db_host: process.env.DB_HOST,
     db_name: process.env.DB_NAME,
-    key: "5NLMmX8DuhrOaWrZfTbyxoyqM3k5WjFYvjXvGu5Ek7dPAPNgElFnrD8Z_pVtJq_ZrDYQvVqJyW58Ygvf0KhtqLPNu5nhs805Lzz4lBOdjGqRt8Y_CgBc3YKivVLNmoVTIFwD9-FFlgRYAra1uC2Q56Wp5egK37LphLle58Pn83U"
+    key: process.env.JWT_KEY
 };
 
 module.exports = { config };
